8bc36201d8445fe74073f7028363b5b76bedca53
bashirmarj <bashirmarj@gmail.com>
2025-11-06 01:14:56 -0500

    Update app.py

--- geometry-service/app.py
+++ geometry-service/app.py
@@ -16,7 +16,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from supabase import create_client, Client
 import logging
-from contextmanager import contextmanager
+from contextlib import contextmanager
 from functools import wraps
 
 # === OCC imports ===
